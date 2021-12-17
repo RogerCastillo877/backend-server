4b26cf51dc62a452fa85eba3fc3b2e4eed226baf
Roger Jamil Castillo Gomez <roger.castillo877@gmail.com>
2021-12-17 11:33:24 -0500

    First route

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+// Routes
+app.get( '/', (req, res) => {
+    
+    res.json({
+        ok:true,
+        msg: 'Hola...'
+    })
+});
+
 app.listen( 3000, () => {
     
 })
